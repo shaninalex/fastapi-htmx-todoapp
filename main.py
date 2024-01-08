e0e49cfaf25c9f76c01d89cb7760d5577b6f2422
shaninalex <shanin.x@gmail.com>
2024-01-08 16:47:25 +0200

    patch: add cors middleware and example custom middleware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import time
 from typing import Annotated
 from fastapi import FastAPI, Request, Form
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -19,7 +21,24 @@ async def lifespan(app: FastAPI):
     await database.disconnect()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Middleware example
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 templates = Jinja2Templates(directory="templates")
 
