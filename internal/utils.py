c0a8406a69b01ca520887c0d9c39b5d6363dbddd
shaninalex <shanin.x@gmail.com>
2024-01-09 11:33:32 +0200

    patch: login/register

--- internal/utils.py
+++ internal/utils.py
@@ -1,17 +1,49 @@
-from fastapi import Request, Response
+from fastapi import Request, Response, HTTPException
+from argon2 import PasswordHasher
 import datetime
 import jwt
+from .db import user, database
+
 
 def generate_auth_cookie(email: str) -> str:
-    auth_token = jwt.encode({
-        "email": email,
-        "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=30),
-    }, "secret", algorithm="HS256")
+    auth_token = jwt.encode(
+        {
+            "email": email,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+        },
+        "secret",
+        algorithm="HS256",
+    )
     return auth_token
 
 
 def validate_user(request: Request, response: Response):
-    auth_token = request.cookies.get('auth')
+    auth_token = request.cookies.get("auth")
     if not auth_token:
-        print("not authenticated. Redirect to auth page")
-    print(auth_token)
+        raise HTTPException(
+            status_code=303,
+            detail="Redirecting to /auth",
+            headers={"Location": "/auth"},
+        )
+    try:
+        payload = jwt.decode(auth_token, "secret", algorithms=["HS256"])
+        query = select([user.c.email]).where(user.c.email == email)
+        print(payload["email"])
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=303,
+            detail="Redirecting to /auth",
+            headers={"Location": "/auth"},
+        )
+
+
+def generate_password(password: str) -> str:
+    ph = PasswordHasher()
+    return str(ph.hash(password.encode("utf-8")))
+
+
+def check_password(password: str, encoded_password: str):
+    ph = PasswordHasher()
+    return ph.verify(encoded_password, password)
--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import time
 from typing import Annotated
 from fastapi import Depends, FastAPI, Request, Form, Response
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from contextlib import asynccontextmanager
 
-from db import metadata, engine, database, user
-from internal.utils import generate_auth_cookie, validate_user
+from internal.db import metadata, engine, database, user
+from internal.utils import (
+    generate_auth_cookie,
+    generate_password,
+    validate_user,
+    check_password,
+)
 
 
 metadata.create_all(engine)
@@ -57,7 +62,9 @@ async def home(request: Request):
 
 @app.get("/auth", response_class=HTMLResponse)
 async def auth_page(request: Request):
-    return templates.TemplateResponse(request=request, name="auth.html", context={"title": "Login"})
+    return templates.TemplateResponse(
+        request=request, name="auth.html", context={"title": "Login"}
+    )
 
 
 @app.post("/auth")
@@ -68,15 +75,55 @@ async def auth_handler(
     password: Annotated[str, Form()],
 ):
     try:
+        query = select([user.c.password]).where(user.c.email == email)
+        account = await database.fetch_one(query)
+        check_password(password, account.password)
+        response.set_cookie(
+            "auth", generate_auth_cookie(email), secure=True, httponly=True
+        )
+        response.headers["HX-Location"] = "/"
+    except Exception as err:
+        return templates.TemplateResponse(
+            request=request,
+            name="components/alert.html",
+            context={
+                "type": "danger",
+                "message": err,
+            },
+        )
+
+
+@app.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="auth.html",
+        context={"title": "Register", "register": True},
+    )
+
+
+@app.post("/register")
+async def register_handler(
+    request: Request,
+    response: Response,
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+):
+    try:
         query = insert(user).values(
-            email=email, name="test", password="aadflkjadf", created_at=datetime.now()
+            email=email,
+            name="test",
+            password=generate_password(password),
+            created_at=datetime.now(),
         )
         await database.execute(query)
-        token = generate_auth_cookie(email)
-        response.set_cookie("auth", token, secure=True, httponly=True)
+        response.set_cookie(
+            "auth", generate_auth_cookie(email), secure=True, httponly=True
+        )
         response.headers["HX-Location"] = "/"
         return {"status": "ok"}
-    except:
+    except Exception as e:
+        print(e)
         return templates.TemplateResponse(
             request=request,
             name="components/alert.html",
@@ -87,12 +134,15 @@ async def auth_handler(
         )
 
 
-@app.get("/register", response_class=HTMLResponse)
-async def register_page(request: Request):
-    return templates.TemplateResponse(request=request, name="auth.html", context={
-        "title": "Register",
-        "register": True
-    })
+@app.get("/logout")
+def logout(response: Response):
+    # Delete the "auth" cookie
+    response.delete_cookie(key="auth", path="/", secure=True, httponly=True)
+    # Redirect the user to the home page
+    response.headers["Location"] = "/"
+    response.status_code = 302  # 302 Found (temporary redirect)
+    return response
+
 
 @app.post("/clicked", response_class=HTMLResponse)
 async def clicked(request: Request):
